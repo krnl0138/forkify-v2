7b38c569e811cf529b656e2e7882dd16bc1914ca
krnl0138 <krnl03182@gmail.com>
2021-04-28 22:36:38 +0300

    Removed new feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -96,10 +96,6 @@ const controlAddRecipe = async function(newRecipe) {
   }
 };
 
-const newFeature = function() {
-  console.log('wow, hello world!');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -108,6 +104,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
