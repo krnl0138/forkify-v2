67d842d4e7b5d05244c886fd35398ed21bde6770
krnl0138 <krnl03182@gmail.com>
2021-04-28 22:31:17 +0300

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -96,6 +96,10 @@ const controlAddRecipe = async function(newRecipe) {
   }
 };
 
+const newFeature = function() {
+  console.log('wow, hello world!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -104,5 +108,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
